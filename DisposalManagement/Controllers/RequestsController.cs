7cfbc54e07c6e4a0a871fc124704fdbca123abf7
eliasp9 <elias.papanikitas@hotmail.com>
2016-12-22 15:38:35 +0100

    request message altered

--- DisposalManagement/Controllers/RequestsController.cs
+++ DisposalManagement/Controllers/RequestsController.cs
@@ -81,7 +81,7 @@ namespace DisposalManagement.Controllers
             }
 
             Sender sender = new Sender();
-            sender.send(request.ClientEmail, "Uw aanvraag is aangekregen","Uw aanvrag is aangekomen en zal zo snel mogelijk verwerkt worden.");
+            sender.send(request.ClientEmail, "Uw aanvraag is ontvangen","Wij hebben uw aanvraag ontvangen en zal zo snel mogelijk verwerkt worden.");
             db.Requests.Add(request);
             db.SaveChanges();
 
