ae748efbd1a2f2fd760266f5fd9819cedda2f9a6
eliasp9 <elias.papanikitas@hotmail.com>
2016-12-22 14:24:14 +0100

    request added

---
+++ DisposalManagement/Models/Request.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DisposalManagement.Models
+{
+    public class Request
+    {
+        public int ClientId { get; set; }
+        public string ClientName { get; set; }
+        public string ClientEmail { get; set; }
+        public string ClientPostalCode { get; set; }
+        public bool Intern { get; set; }
+        public string ContainerId { get; set; }
+    }
+}
