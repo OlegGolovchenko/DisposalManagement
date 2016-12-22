48d14dd9ec4c4ffccbcdb9b3541b44f5c94f2247
OlegGolovchenko <igrok_be@hotmail.com>
2016-12-22 14:36:38 +0100

    added requests controller

---
+++ DisposalManagement/Models/DisposalManagementContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DisposalManagement.Models
+{
+    public class DisposalManagementContext : DbContext
+    {
+        // You can add custom code to this file. Changes will not be overwritten.
+        // 
+        // If you want Entity Framework to drop and regenerate your database
+        // automatically whenever you change your model schema, please use data migrations.
+        // For more information refer to the documentation:
+        // http://msdn.microsoft.com/en-us/data/jj591621.aspx
+    
+        public DisposalManagementContext() : base("name=DisposalManagementContext")
+        {
+        }
+
+        public System.Data.Entity.DbSet<DisposalManagement.Models.Request> Requests { get; set; }
+    }
+}
